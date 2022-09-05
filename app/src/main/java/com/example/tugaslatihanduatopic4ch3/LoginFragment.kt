d62b78f250a8ff5f91784ab7894f7fdc3ce02b68
RizalRahmadani <rizalrahmadani98@gmail.com>
2022-09-05 23:54:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugaslatihanduatopic4ch3/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.example.tugaslatihanduatopic4ch3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnLogin.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+        textLupaPassword.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_lupaPaswordFragment)
+        }
+        textRegister.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        btnBantuan.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_helpFragment)
+        }
+        btnChats.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_chatsFragment)
+        }
+
+    }
+
+}
